11e4b29458c8397211a54acca68de97154e312f4
Anas Raza <ar470684@gmail.com>
2026-07-18 10:05:00 +0500

    did as many as possible

---
+++ app.js
@@ -0,0 +1,86 @@
+// // CHAPTER 1
+// // 1.
+// alert("Anas");
+// alert("Raza");
+// alert("ar470684@gmail.com");
+// alert("0324-1234567");
+// alert("password is password");
+
+// // 2.
+// alert("You're learning JavaScript!");
+
+// // 3.
+// alert("I like this message.");
+
+// // CHAPTER 2
+// // 1.
+// var camelCase; // declaration only
+
+// // 2.
+// var myName;
+// myName = "Anas"
+
+// // 3.
+// var teamName = "My team";
+// alert(teamName);
+
+// // 4.
+// bestMan = "New charlie";
+
+// // CHAPTER 3
+// // 1. 
+// var caseQty;
+
+// // 2.
+// caseQty = 144;
+
+// // 3.
+// var num = 9;
+
+// // 4.
+// var sum;
+// sum = 1 + 2;
+
+// // 5.
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// alert(orderTotal);
+
+// // 6.
+// var number = 2;
+// number = number + 3;
+
+// // CHAPTER 4
+// // 1.
+// var one, two, three;
+
+// // 2.
+// var first, secondVar, ThirdVar3, _fourth_var, $fifth; // legal
+// // var 2first, @anas, my variable, var, 123; // illegal
+
+// // 3.
+// document.write("<h1>Rules for naming JS variables</h1> <br>");
+// document.write("Variable names can only contain numbers, $ and _ . ")
+// document.write("For example $my_1stVariable <br>")
+// document.write("Variables must begin with a letter, $ or _. For example $name, _name or name <br>");
+// document.write("Variable names are case sensitive <br>");
+// document.write("Variable names should not be JS keywords")
+
+// CHAPTER 5
+// 1.
+var num1 = +prompt("Enter number One");
+var num2 = +prompt("Enter number Two");
+var sum = num1 + num2;
+document.write("Sum of " + num1 + " and " + num2 + " is " + sum + "<br>");
+
+// 2.
+var diff = num1 - num2;
+document.write("Difference of " + num1 + " and " + num2 + " is " + diff + "<br>");
+var product = num1 * num2;
+document.write("Product of " + num1 + " and " + num2 + " is " + product + "<br>");
+var quotient = num1 / num2;
+document.write("Quotient of " + num1 + " and " + num2 + " is " + quotient + "<br>");
+var remainder = num1 % num2;
+document.write("Remainder of " + num1 + " and " + num2 + " is " + remainder);
+
